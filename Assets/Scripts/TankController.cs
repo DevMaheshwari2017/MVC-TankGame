f304df8435d364afb8da39561017116cb4f8dfe6
Dev <devmaheswari2017@gmail.com>
2024-04-08 14:54:53 +0530

    Created MVC for tank and also added movement and rotation

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -3,15 +3,27 @@ public class TankController
 {
     private TankModel tankModel;
     private TankView tankView;
+    private Rigidbody rb;
 
     public TankController(TankModel _tankModel, TankView _tankView)
     {
         tankModel = _tankModel;
-        tankView = _tankView;
+        tankView = GameObject.Instantiate<TankView>(_tankView);
+        rb = tankView.GetRigidbody();
 
         tankModel.SetTankController(this);
         tankView.SetTankController(this);
+    }
 
-        GameObject.Instantiate(tankView.gameObject);
+    public void Move(float Movement, float MovementSpeed)
+    {
+        rb.velocity = tankView.transform.forward * Movement * MovementSpeed;
+    }
+
+    public void Roatate(float Rotate, float RotationSpeed)
+    {
+        Vector3 rotatio = new Vector3(0f, Rotate * RotationSpeed, 0f);
+        Quaternion deltaRotation = Quaternion.Euler(rotatio * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
     }
 }
--- Assets/Scripts/TankView.cs
+++ Assets/Scripts/TankView.cs
@@ -4,7 +4,33 @@ using UnityEngine;
 
 public class TankView : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody rb;
+
     private TankController tankController;
+    private float movement;
+    private float rotate;
+
+
+    private void Update()
+    {
+        Movement();
+
+        if (movement != 0)
+            tankController.Move(movement, 30);
+        if (rotate != 0)
+            tankController.Roatate(rotate, 40);
+    }
+
+    private void Movement() 
+    {
+        movement = Input.GetAxis("Vertical");
+        rotate = Input.GetAxis("Horizontal");
+    }
+    public Rigidbody GetRigidbody() 
+    {
+        return rb;
+    }
     public void SetTankController(TankController _tankController)
     {
         tankController = _tankController;
