d80c50ca89cb29ed3084f07aac950149755e2f62
Dev <devmaheswari2017@gmail.com>
2024-04-08 16:59:15 +0530

    Added Tank selection panel

--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -18,13 +18,22 @@ public class TankSpawner : MonoBehaviour
     [SerializeField]
     private TankView tankView;
 
-    private void Start()
-    {
-        CreateTank();
-    }
+    public void CreateTank(TankTypes tankType) {
 
-    private void CreateTank() {
-        TankModel tankModel = new TankModel(tank[2].movementSpeed, tank[2].rotationSpeed, tank[2].tankType, tank[2].color);
-        TankController tankController = new TankController(tankModel ,tankView);
+        if (tankType == TankTypes.BlueTank)
+        {
+            TankModel tankModel = new TankModel(tank[2].movementSpeed, tank[2].rotationSpeed, tank[2].tankType, tank[2].color);
+            TankController tankController = new TankController(tankModel, tankView);
+        }
+        else if (tankType == TankTypes.RedTank)
+        {
+            TankModel tankModel = new TankModel(tank[1].movementSpeed, tank[1].rotationSpeed, tank[1].tankType, tank[1].color);
+            TankController tankController = new TankController(tankModel, tankView);
+        }
+        else if (tankType == TankTypes.GreenTank) 
+        {
+            TankModel tankModel = new TankModel(tank[0].movementSpeed, tank[0].rotationSpeed, tank[0].tankType, tank[0].color);
+            TankController tankController = new TankController(tankModel, tankView);
+        }
     }
 }
